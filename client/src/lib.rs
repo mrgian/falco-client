690dbe0b07ca157b452d11edf33ea33c82660669
mrgian <mail@gian.im>
2023-05-16 07:34:26 +0200

    Add libs

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,14 +1,10 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use falco::{service_client::ServiceClient, Request};
+use tonic::transport::{Certificate, Channel, ClientTlsConfig, Identity};
+
+pub mod falco {
+    tonic::include_proto!("falco.outputs");
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
 }
